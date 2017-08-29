7185bd45afde244f9119bdb1235428bf02eec470
Robin <robin@digi.ninja>
2017-08-29 10:08:54 +0100

    starting out-of-band module

---
+++ vulnerabilities/oob/help/help.php
@@ -0,0 +1,42 @@
+<div class="body_padded">
+	<h1>Help - Out-of-Band Vulnerabilities</h1>
+
+	<div id="code">
+		<table width='100%' bgcolor='white' style="border:2px #C0C0C0 solid">
+		<tr>
+		<td><div id="code">
+			<h3>About</h3>
+			<p></p>
+
+			<h3>Objective</h3>
+			<p></p>
+
+			<p><hr /></p>
+
+			<h3>Low Level</h3>
+			<p></p>
+
+			<p><br /></p>
+
+			<h3>Medium Level</h3>
+			<p></p>
+
+			<p><br /></p>
+
+			<h3>High Level</h3>
+			<p></p>
+
+			<p><br /></p>
+
+			<h3>Impossible Level</h3>
+			<p></p>
+		</div></td>
+		</tr>
+		</table>
+
+	</div>
+
+	<br />
+
+	<p>Reference: <?php echo dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/Cross-site_Scripting_(XSS)' ); ?></p>
+</div>
